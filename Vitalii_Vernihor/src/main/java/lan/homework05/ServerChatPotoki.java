4af3bbbbc535c11916f6bac01b93990d06957e40
Taska23 <77rex7@gmail.com>
2018-10-10 01:16:24 +0300

    Закоммитил какую-то дичь

---
+++ Vitalii_Vernihor/src/main/java/lan/homework05/ServerChatPotoki.java
@@ -0,0 +1,63 @@
+package lan.homework05;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerChatPotoki {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        channel.bind(new InetSocketAddress(20000));
+        System.out.println("Сервер запущен, ожидается подключения клиента...");
+        SocketChannel socket = channel.accept();
+        System.out.println("Соединение установленно...");
+        ByteBuffer buffer = ByteBuffer.allocate(128);
+
+
+        Runnable chitalka = () -> {
+          try {
+              int bytes;
+              while ((bytes = socket.read(buffer)) > 0 ){
+
+                  buffer.flip();
+
+                  System.out.println("Входящее сообщение: "
+                          + new String(buffer.array(),0,bytes));
+
+                  buffer.clear();
+              }
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+
+
+        };
+
+        Runnable pisalka = () -> {
+            try{
+                while (true){
+                    String text = scan.nextLine();
+
+                    buffer.put(text.getBytes());
+                    buffer.flip();
+                    socket.write(buffer);
+                    buffer.clear();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        es.execute(chitalka);
+        es.execute(pisalka);
+    }
+}
--- Vitalii_Vernihor/src/main/java/lan/homework06/lanChatMultiUserServer.java
+++ Vitalii_Vernihor/src/main/java/lan/homework06/lanChatMultiUserServer.java
@@ -1,4 +1,4 @@
-package lan.lesson05;
+package lan.homework06;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -9,34 +9,48 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class ServerChatPotoki {
+public class lanChatMultiUserServer {
     public static void main(String[] args) throws IOException {
 
         Scanner scan = new Scanner(System.in);
 
         ServerSocketChannel channel = ServerSocketChannel.open();
         channel.bind(new InetSocketAddress(20000));
-        System.out.println("Сервер запущен, ожидается подключения клиента...");
+        System.out.println("Запуск сервера...");
         SocketChannel socket = channel.accept();
         System.out.println("Соединение установленно...");
         ByteBuffer buffer = ByteBuffer.allocate(128);
 
 
+        Runnable clientListener = () ->{
+            System.out.println("Запуск модуля подключения новых пользователей");
+            while (true) {
+
+                try {
+                    channel.accept();
+                    System.out.println("Соединение установленно...");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
         Runnable chitalka = () -> {
-          try {
-              int bytes;
-              while ((bytes = socket.read(buffer)) > 0 ){
+            try {
+                int bytes;
+                while ((bytes = socket.read(buffer)) > 0 ){
 
-                  buffer.flip();
+                    buffer.flip();
 
-                  System.out.println("Входящее сообщение: "
-                          + new String(buffer.array(),0,bytes));
+                    System.out.println("Входящее сообщение: "
+                            + new String(buffer.array(),0,bytes));
 
-                  buffer.clear();
-              }
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
+                    buffer.clear();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 
         };
@@ -56,7 +70,8 @@ public class ServerChatPotoki {
             }
         };
 
-        ExecutorService es = Executors.newFixedThreadPool(2);
+        ExecutorService es = Executors.newFixedThreadPool(3);
+        es.execute(clientListener);
         es.execute(chitalka);
         es.execute(pisalka);
     }
