1c6c1cc79db3381b6ab842f95b4f7508257f2b7c
Ярослав <kaon1999@gmail.com>
2018-10-11 21:18:14 +0300

    Modificate

---
+++ Yaroslav_Halindzovskiy/src/main/java/chatHomework/Client.java
@@ -0,0 +1,108 @@
+package chatHomework;
+
+import com.sun.javafx.image.impl.ByteRgb;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client {
+    private static SocketChannel socketChannel;
+   private static ByteBuffer buffer = ByteBuffer.allocate(128);
+
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        connect();
+        Runnable runnableWrite = (() -> {
+            while (true){
+            writter(sc,socketChannel);}
+
+        });Runnable runnableRead = (() -> {
+            while (true) {
+                reader(socketChannel);
+            }
+        });
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        es.execute(runnableRead);
+        es.execute(runnableWrite);
+        while (true) {
+            try {
+                if (socketChannel.finishConnect()) {
+                    es.shutdown();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public static void connect() {
+        try {
+            socketChannel = SocketChannel.open(new InetSocketAddress(50005));
+            if (socketChannel.isConnected()){
+                System.out.println("Соединение с сервером устаановлено.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+    public static void reader (SocketChannel channel){
+        while (true){
+        int bytes;
+
+
+        try{
+            while ((bytes = channel.read(buffer))>0){
+
+                buffer.flip();
+                System.out.println("Входящее сообщение " + new String(buffer.array()));
+            buffer.clear();
+            }
+
+        }catch (IOException e ){
+
+        }
+        catch (BufferOverflowException r) {
+        }
+        }
+
+    }
+    public static void writter (Scanner sc ,SocketChannel channel) {
+
+        System.out.println("Сообщение для отправки");
+        String text = sc.nextLine();
+        if (text.equals("exit")){
+            try {
+                channel.shutdownInput();
+                channel.shutdownOutput();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        buffer.put(text.getBytes());
+        buffer.flip();
+        try {
+            channel.write(buffer);
+            buffer.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
+
+
