cc325322a2fb66e2dea65ba298e2bdf3377ac4b0
Ярослав <kaon1999@gmail.com>
2018-10-13 19:06:25 +0300

    Modificate

--- Yaroslav_Halindzovskiy/src/main/java/chatHomework/ServerSecond.java
+++ Yaroslav_Halindzovskiy/src/main/java/chatHomework/ServerSecond.java
@@ -1,17 +1,20 @@
+package chatHomework;
 
 import java.io.IOException;
 
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ServerSecond {
-    static List<SocketChannel> socketChannelList = new ArrayList<>();
+    static List<SocketChannel> socketChannelList = Collections.synchronizedList(new ArrayList<>());
     static ExecutorService es = Executors.newCachedThreadPool();
     static ByteBuffer buffer = ByteBuffer.allocate(128);
 
@@ -22,16 +25,17 @@ public class ServerSecond {
 try {
     ServerSocketChannel server = ServerSocketChannel.open();
     server.bind(new InetSocketAddress(50006));
-    Runnable runnable1 = () -> {
+
         do {
-            writeAndRead();
-        }while (server.isOpen());
-    };
+            for (SocketChannel socketChannel:socketChannelList){
+            writeAndRead(socketChannel);
+        }}while (server.isOpen());
+
 
     do {
         SocketChannel client = server.accept();
         es.execute(new MonoTreadClient(client));
-        es.execute(runnable1);
+
 
     } while (true);
 }catch (IOException e ){}
@@ -39,27 +43,29 @@ try {
     }
 
 
-    public static void writeAndRead (){
+    public static void writeAndRead (SocketChannel socketChannel){
 
         try {
 
-            for (SocketChannel socketChannel:socketChannelList){
+
             int bytes;
-            if ((bytes = socketChannel.read(ServerSecond.buffer)) > 0) {
+            //if ((bytes = socketChannel.read(ServerSecond.buffer)) > 0) {
+
+socketChannel.read(buffer);
 
                 ServerSecond.buffer.flip();
 
                 System.out.println("Входящее сообщение: "
-                        + new String(ServerSecond.buffer.array(), 0, bytes));
+                        + new String(ServerSecond.buffer.array()));
                 for (SocketChannel s : ServerSecond.socketChannelList) {
                     s.write(ServerSecond.buffer);
                 }
 
 
                 ServerSecond.buffer.clear();
-                bytes = 0;
-            }
-            }
+
+
+           // }
         } catch (IOException e){}
 
 
