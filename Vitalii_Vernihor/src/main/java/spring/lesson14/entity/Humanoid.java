6eae6eac426b87a10f2ee30492b1dfd813b764cc
Taska23 <77rex7@gmail.com>
2018-11-11 16:47:03 +0200

    ls14

--- Vitalii_Vernihor/src/main/java/spring/lesson14/entity/Humanoid.java
+++ Vitalii_Vernihor/src/main/java/spring/lesson14/entity/Humanoid.java
@@ -21,4 +21,38 @@ public class Humanoid {
         this.model = model;
         this.price = price;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Humanoid{");
+        sb.append("id=").append(id);
+        sb.append(", model='").append(model).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        return sb.toString();
+    }
 }
