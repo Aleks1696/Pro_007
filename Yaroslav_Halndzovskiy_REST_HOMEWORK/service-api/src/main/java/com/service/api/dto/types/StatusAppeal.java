af7268c4637bf9ed5b3be8bfd2f8d5657fa7be43
Ярослав <kaon1999@gmail.com>
2018-11-21 23:00:43 +0200

    Modificate

---
+++ Yaroslav_Halndzovskiy_REST_HOMEWORK/service-api/src/main/java/com/service/api/dto/types/StatusAppeal.java
@@ -0,0 +1,5 @@
+package com.service.api.dto.types;
+
+public enum StatusAppeal {
+    ACCEPTED, IN_PROCESSING , REVIEWED
+}
---
+++ Yaroslav_Halndzovskiy_REST_HOMEWORK/service-core/src/main/java/com/service/core/endpoint/AppealRestEnpoint.java
@@ -0,0 +1,59 @@
+package com.service.core.endpoint;
+
+
+import com.service.api.dto.AppealDTO;
+import com.service.api.dto.types.StatusAppeal;
+import com.service.api.endpoint.AppealEndpoint;
+import com.service.api.request.CreateAppealRequest;
+import com.service.api.request.UpdateAppealRequest;
+import com.service.api.response.GeneralResponse;
+import com.service.core.service.AppealService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AppealRestEnpoint implements AppealEndpoint {
+    private AppealService appealService;
+@Autowired
+    public AppealRestEnpoint(AppealService appealService) {
+        this.appealService = appealService;
+    }
+
+    public GeneralResponse<Long> createAppeal(@RequestBody CreateAppealRequest request) {
+        return new GeneralResponse<Long>("200",appealService.create(request));
+    }
+
+    public GeneralResponse<AppealDTO> getAppealById(@RequestParam("id") Long id) {
+        return new GeneralResponse<AppealDTO>("200",appealService.get(id));
+    }
+
+    public GeneralResponse<Void> updateAppeal(@RequestBody UpdateAppealRequest request) {
+    appealService.update(request);
+        return  new GeneralResponse<Void>("200",null);
+    }
+
+    public GeneralResponse<Void> deleteAppeal(@RequestParam("id") Long id) {
+    appealService.delete(id);
+        return new GeneralResponse<Void>("200",null);
+    }
+
+    public void changeStatus(@RequestParam Long id, StatusAppeal statusAppeal) {
+appealService.changeStatus(id,statusAppeal);
+    }
+
+    public GeneralResponse<List<AppealDTO>> getAll() {
+
+        return new GeneralResponse<List<AppealDTO>>("200",null);
+
+    }
+
+    public GeneralResponse<List<AppealDTO>> getPage() {
+        return null;
+    }
+
+
+}
