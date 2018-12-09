02b22bff2112cc8c43936792910100c83d1f0f3b
egboldyr <evgenboldyr@gmail.com>
2018-12-09 16:16:56 +0200

    Aspects part 1

--- Pro007_Examples/src/main/java/spring/lesson22/AspectsApp.java
+++ Pro007_Examples/src/main/java/spring/lesson22/AspectsApp.java
@@ -1,8 +1,21 @@
 package spring.lesson22;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.lesson22.dao.CarDAO;
+import spring.lesson22.entity.Car;
+
 public class AspectsApp {
 
     public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext(
+                        "spring/lesson22/config.xml"
+                );
+        CarDAO carDAO = context.getBean(CarDAO.class);
 
+        Car car = new Car("Kia", "Black", 2010);
+
+        carDAO.create(car);
     }
 }
